a9025f73992b9fadb48dcf1169b7ed284fdb9451
TanviS <s0578815@htw-berlin.de>
2021-07-19 17:54:49 +0200

    Unit Test Set Up

---
+++ UnitTest1/ImportFeatureTesting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using QLifeC_Datatool;
+
+namespace UnitTest
+{
+    public class ImportFeatureTesting
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            double expected = 13.0;
+
+            //Act
+            double actual = 13.0;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
