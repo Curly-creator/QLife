458762c02ca454df9bb94f3b39895f95d9b47dba
Lor <s0579237@htw-berlin.de>
2021-07-21 00:04:05 +0200

    Function Sum Up In Loops

--- QLifeC_Datatool/AddCity.xaml.cs
+++ QLifeC_Datatool/AddCity.xaml.cs
@@ -38,53 +38,43 @@ namespace QLifeC_Datatool
             {
                 cityToBeAdded.Categories[i].Score.ScoreOutOf10 = allSliders[i].Value;
             }
-            
+
             //Adding Numbers in Data/Subcategories for Cat[0]/col.
             List<Label> cat0labellist = new List<Label> { col1_lb, col2_lb, col3_lb, col4_lb, col5_lb, col6_lb, col7_lb, col8_lb, col9_lb, col10_lb, col11_lb };
             List<TextBox> cat0tblist = new List<TextBox> { col1_tb, col2_tb, col3_tb, col4_tb, col5_tb, col6_tb, col7_tb, col8_tb, col9_tb, col10_tb, col11_tb };
-            for (int i=0; i<11; i++)
-            {
-                AddSubcategory(0, cat0labellist[i].Content.ToString().Remove(cat0labellist[i].Content.ToString().Length-1), double.Parse(cat0tblist[i].Text));     //so complicated because 'Inflation::' -> 'Inflation:'
-            }
 
             //Adding Numbers in Data/Subcategories for Cat[1]/col.
             List<Label> cat1labellist = new List<Label> {h1_lb, h2_lb, h3_lb, h4_lb };
             List<TextBox> cat1tblist = new List<TextBox> { h1_tb, h2_tb, h3_tb, h4_tb };
-            for (int i = 0; i < 4; i++)
-            {
-                AddSubcategory(1, cat1labellist[i].Content.ToString().Remove(cat1labellist[i].Content.ToString().Length - 1), double.Parse(cat1tblist[i].Text));     //so complicated because 'Inflation::' -> 'Inflation:'
-            }
             
             //Adding Numbers in Data/Subcategories for Cat[2]/ia.
             List<Label> cat2labellist = new List<Label> { ia1_lb, ia2_lb, ia3_lb, ia4_lb };
             List<TextBox> cat2tblist = new List<TextBox> { ia1_tb, ia2_tb, ia3_tb, ia4_tb };
-            for (int i = 0; i < 4; i++)
-            {
-                AddSubcategory(2, cat2labellist[i].Content.ToString().Remove(cat2labellist[i].Content.ToString().Length - 1), double.Parse(cat2tblist[i].Text));     //so complicated because 'Inflation::' -> 'Inflation:'
-            }
             
             //Adding Numbers in Data/Subcategories for Cat[3]/eq.
             List<Label> cat3labellist = new List<Label> { eq1_lb, eq2_lb, eq3_lb, eq4_lb };
             List<TextBox> cat3tblist = new List<TextBox> {eq1_tb, eq2_tb, eq3_tb, eq4_tb };
-            for (int i = 0; i < 4; i++)
-            {
-                AddSubcategory(3, cat3labellist[i].Content.ToString().Remove(cat3labellist[i].Content.ToString().Length - 1), double.Parse(cat3tblist[i].Text));     //so complicated because 'Inflation::' -> 'Inflation:'
-            }
             
             //Adding Numbers in Data/Subcategories for Cat[4]/tc.
             List<Label> cat4labellist = new List<Label> { tc1_lb, tc2_lb, tc3_lb };
             List<TextBox> cat4tblist = new List<TextBox> { tc1_tb, tc2_tb, tc3_tb };
-            for (int i = 0; i < 3; i++)
-            {
-                AddSubcategory(4, cat4labellist[i].Content.ToString().Remove(cat4labellist[i].Content.ToString().Length - 1), double.Parse(cat4tblist[i].Text));     //so complicated because 'Inflation::' -> 'Inflation:'
-            }
             
             //Adding Numbers in Data/Subcategories for Cat[5]/o.
             List<Label> cat5labellist = new List<Label> { o1_lb, o2_lb, o3_lb, o4_lb, o5_lb, o6_lb, o7_lb };
             List<TextBox> cat5tblist = new List<TextBox> { o1_tb, o2_tb, o3_tb, o4_tb, o5_tb, o6_tb, o7_tb };
-            for (int i = 0; i < 7; i++)
+
+            //creme de la creme: loop in loop
+            List<List<Label>> catLabelListOfList = new List<List<Label>> { cat0labellist, cat1labellist, cat2labellist, cat3labellist, cat4labellist, cat5labellist };
+            List<List<TextBox>> catTextBoxListOfList = new List<List<TextBox>> { cat0tblist, cat1tblist, cat2tblist, cat3tblist, cat4tblist, cat5tblist };
+            for (int j = 0; j < catLabelListOfList.Count; j++)
             {
-                AddSubcategory(5, cat5labellist[i].Content.ToString().Remove(cat5labellist[i].Content.ToString().Length - 1), double.Parse(cat5tblist[i].Text));     //so complicated because 'Inflation::' -> 'Inflation:'
+                for (int i = 0; i < catLabelListOfList[j].Count; i++)
+                {
+                    AddSubcategory(j, catLabelListOfList[j][i].Content.ToString().Remove(catLabelListOfList[j][i].Content.ToString().Length - 1), double.Parse(catTextBoxListOfList[j][i].Text));     //so complicated because 'Inflation::' -> 'Inflation:'
+                //j counts from 0-5 to go through the 6 big Categories =indexOfCat
+                //catLabelListOfList[j].Count is how many subcategories (11,4,4,4,3,7) the current cat has
+                //the [i] goes through the 11 subcategories and adds them to the city'Datensatz'
+                } 
             }
 
             ((MainWindow)Application.Current.MainWindow).cityList.Add(cityToBeAdded); //here the city is manually added to your testCityList
