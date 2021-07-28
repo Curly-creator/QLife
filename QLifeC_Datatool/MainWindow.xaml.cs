e8290a04069224ec8be4c87cf09fc2d4ec6908a8
TanviS <s0578815@htw-berlin.de>
2021-07-28 18:32:35 +0200

    Added import methods to unsuccessful merge in the previous commit.

--- QLifeC_Datatool/MainWindow.xaml.cs
+++ QLifeC_Datatool/MainWindow.xaml.cs
@@ -50,5 +50,78 @@ namespace QLifeC_Datatool
             Dgd_MainGrid.ItemsSource = cityList;
             Dgd_MainGrid.Items.Refresh();
         }
+
+        /// <summary>
+        /// This is the event handler for "Import" button click. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Import_Click(object sender, RoutedEventArgs e)
+        {
+            //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.openfiledialog?view=net-5.0
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    //Getting the path and extension of the selected file to be imported.
+                    string FilePath = openFileDialog.FileName;
+                    string FileExt = System.IO.Path.GetExtension(FilePath).ToLower();
+
+                    //Calling the import method to initiate file import.
+                    CheckFileExtandImport(FileExt, FilePath);
+                }
+                //Refreshing the datagrid view with the udpated list after import.
+                Dgd_MainGrid.ItemsSource = cityList;
+                Dgd_MainGrid.Items.Refresh();
+            }
+        }
+        /// <summary>
+        /// The method takes type of file (extension) and the path of the file to be imported. Checks if it is the right format and imports.
+        /// </summary>
+        /// <param name="FileExt"></param>
+        /// <param name="FilePath"></param>
+        public void CheckFileExtandImport(string FileExt, string FilePath)
+        {
+            ITarget target;
+            try
+            {
+                //File Type is XML.
+                if (FileExt == FileTypeAllowed[0])
+                {
+                    target = new AdapterXML();
+                    target.CallImportAdapter(FileExt, FilePath);
+                    cityList = target.cityList;
+                    MessageBox.Show(target.StatusNotification, "Import Window", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                //File Type is CSV.
+                else if (FileExt == FileTypeAllowed[1])
+                {
+                    target = new AdapterCSV();
+                    target.CallImportAdapter(FileExt, FilePath);
+                    cityList = target.cityList;
+                    MessageBox.Show(target.StatusNotification, "Import Window", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                //File Type is invalid.
+                else
+                {
+                    MessageBox.Show("This is not a valid file extension", "Import Window", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                MethodStatus = true;
+            }
+            catch (Exception ex)
+            {
+                ErrorNotification = "Error: " + ex.Message;
+                MethodStatus = false;
+            }
+
+        }
+
     }
 }
